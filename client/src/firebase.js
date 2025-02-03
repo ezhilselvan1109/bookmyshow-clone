076de120a3295186d921db0cf838a41ed962a459
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-02-04 01:09:44 +0530

    auth implementation

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFNVWhuGA_v1-0CczTzqpZSEBC5UQor3A",
+  authDomain: "bookmyshow-3cc85.firebaseapp.com",
+  projectId: "bookmyshow-3cc85",
+  storageBucket: "bookmyshow-3cc85.firebasestorage.app",
+  messagingSenderId: "672510095792",
+  appId: "1:672510095792:web:ea59e76ae6783d29e2e675"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold text-center mb-4 text-slate-800'>
+        Welcome!
+      </h1>
+    </div>
+  );
+}
